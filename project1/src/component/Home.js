73bd0d5f84934d59255e4705066019d4feb8d18d
unknown <ayesha.shaik@ojas-it.com>
2023-02-04 13:30:09 +0530

    home page login form design

--- project1/src/component/Home.js
+++ project1/src/component/Home.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div>
-      hello
-      
-    </div>
-  )
+    return (
+        <div>
+            <form>
+                <div className='center-aligner'>
+                    <div className='form-container'>
+                        {/* <label>username:</label> */}
+                        <input type="text" placeholder='username'></input><br />
+                        {/* <label>password:</label> */}
+                        <input type="password" placeholder='password'></input><br />
+                        <button>submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Home
